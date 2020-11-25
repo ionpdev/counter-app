3f88bb7c70771c12a62a12195b7f388e99421a82
johny2433 <johny.codmw3@gmail.com>
2020-11-25 15:20:22 +0200

    update and app functions

--- src/index.js
+++ src/index.js
@@ -9,22 +9,51 @@ import hh from "hyperscript-helpers";
 
 const { div, button } = hh(h);
 
+// Default value, initial model
 const initModel = 0;
-
-function view(model) {
+//App View Html
+function view(dispatch, model) {
   return div([
     div({ className: "mv2" }, `Count: ${model}`),
-    button(
-      { className: "pv1 ph2 mr2", onclick: () => console.log("+ clicked") },
-      "+"
-    ),
-    button(
-      { className: "pv1 ph2", onclick: () => console.log("- clicked") },
-      "-"
-    ),
+    button({ className: "pv1 ph2 mr2", onclick: () => dispatch("plus") }, "+"),
+    button({ className: "pv1 ph2", onclick: () => dispatch("minus") }, "-"),
   ]);
 }
+//Update
+function update(msg, model) {
+  switch (msg) {
+    case "plus":
+      return model + 1;
+    case "minus":
+      return model - 1;
+    default:
+      return model;
+  }
+}
 
+// impure code below
+function app(initModel, update, view, node) {
+  // model - state of our app
+  let model = initModel;
+  // current view
+  let currentView = view(dispatch, model);
+  // showing the initial app view to the DOM
+  node.appendChild(currentView);
+
+  //   model = update(msg, model)
+  //   currentView = view(model)
+  //   node.replaceChild(currentView)
+  function dispatch() {
+    model = update(msg, model);
+    const updatedView = view(dispatch, model);
+    node.replaceChild(updatedView, currentView);
+    currentView = updatedView;
+  }
+}
+
+//DOM
 const rootNode = document.getElementById("app");
 
-rootNode.appendChild(view(initModel));
+app(initModel, update, view, rootNode);
+
+// rootNode.appendChild(view(update("plus", initModel)));
